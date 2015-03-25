7137909b75bd783051086b47c621500f925a0ce7
01kawa <kmgrbgm@gmail>
2015-03-26 08:01:35 +0900

    xml change, Foundation5

--- packages/sakan/themes/sakan/default.php
+++ packages/sakan/themes/sakan/default.php
@@ -1,22 +1,22 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php'); ?>
-                <?php
-                  $a = new GlobalArea('Bread nav');
-                  $a->display($c);
-                ?>
-                <main>
-<div class="container">
- <div class="row">
-  <div class="<?php echo $c->getPageWrapperClass()?>">
-    <?php
-        $a = new Area('Main');
-        $a->setAreaGridMaximumColumns(12);
-        $a->display($c);
-    ?>
-  </div><!-- /.WrapperClass -->
- </div><!-- /.row -->
-</div><!-- /.container -->
+<?php
+  $a = new GlobalArea('Bread nav');
+  $a->display($c);
+?>
+<main>
+  <div class="container">
+   <div class="row">
+    <div class="<?php echo $c->getPageWrapperClass()?>">
+      <?php
+          $a = new Area('Main');
+          $a->setAreaGridMaximumColumns(12);
+          $a->display($c);
+      ?>
+    </div><!-- /.WrapperClass -->
+   </div><!-- /.row -->
+  </div><!-- /.container -->
 </main>
 
 
