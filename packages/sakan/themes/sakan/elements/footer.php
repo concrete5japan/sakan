d219859a80c5bb7bc3568a54d443e5ba1dfd6b9f
01kawa <kmgrbgm@gmail>
2015-03-26 06:57:34 +0900

    foundation themes

--- packages/sakan/themes/sakan/elements/footer.php
+++ packages/sakan/themes/sakan/elements/footer.php
@@ -6,74 +6,77 @@ defined('C5_EXECUTE') or die("Access Denied."); ?>
 <footer>
  <div id="footArea">
   <div class="container">
-                <?php
-                  $a = new GlobalArea('Footer Site Title');
-                  $a->display($c);
-                ?>
-   <div class="row">
-    <div class="col-sm-9">
-                <?php
-                  $a = new GlobalArea('Footer Navigation');
-                  $a->display($c);
-                ?>
-    </div><!-- /.col-sm-9 -->
-    <div class="col-sm-3">
-                <?php
-                  $a = new GlobalArea('Footer Contact');
-                  $a->display($c);
-                ?>
-    </div><!-- /.col-sm-3 -->
-   </div><!-- /.row -->
+    <div class="row">
+    <?php
+      $a = new GlobalArea('Footer Site Title');
+      $a->display($c);
+    ?>
+      <div class="small-9 columns">
+        <?php
+          $a = new GlobalArea('Footer Navigation');
+          $a->display($c);
+        ?>
+      </div>
+      <div class="small-3 columns">
+        <?php
+          $a = new GlobalArea('Footer Contact');
+          $a->display($c);
+        ?>
+      </div>
+    </div><!-- /.row -->
   </div><!-- /.container -->
  </div><!-- /#footArea -->
       
  <div id="copyArea">
   <div class="container">
-   <div class="row">
-    <div class="col-sm-9">
-                <?php
-                  $a = new GlobalArea('Footer Legal');
-                  $a->display($c);
-                ?>
+    <div class="row">
+      <div class="small-9 columns">
+        <?php
+          $a = new GlobalArea('Footer Legal');
+          $a->display($c);
+        ?>
 
-                <p><?php echo t('Built with <a href="http://www.concrete5.org" class="concrete5">concrete5</a> CMS.')?>
-                
-                    <?php
-                    if (!id(new User)->isLoggedIn()) {
-                        ?>
-                        <a href="<?php echo URL::to('/login')?>">
-                            <?php echo t('Log in') ?>
-                        </a>
-                        <?php
-                    } else {
-                        $token = new Token();
-                        ?>
-                        <form action="<?php echo URL::to('/login', 'logout') ?>">
-                            <?php id(new Token())->output('logout'); ?>
-                            <a href="#" onclick="$(this).closest('form').submit();return false">
-                                <?php echo t('Log out') ?>
-                            </a>
-                        </form>
-                        <?php
-                    }
-                    ?>
-                </p>
-
-    </div><!-- /.col-sm-9 -->
-    <div class="col-sm-3">
-                <?php
-                  $a = new GlobalArea('Footer Social');
-                  $a->display($c);
+        <p><?php echo t('Built with <a href="http://www.concrete5.org" class="concrete5">concrete5</a> CMS.')?>
+        
+            <?php
+            if (!id(new User)->isLoggedIn()) {
                 ?>
-    </div><!-- /.col-sm-3 -->
-   </div><!-- /.row -->
+                <a href="<?php echo URL::to('/login')?>">
+                    <?php echo t('Log in') ?>
+                </a>
+                <?php
+            } else {
+                $token = new Token();
+                ?>
+                <form action="<?php echo URL::to('/login', 'logout') ?>">
+                    <?php id(new Token())->output('logout'); ?>
+                    <a href="#" onclick="$(this).closest('form').submit();return false">
+                        <?php echo t('Log out') ?>
+                    </a>
+                </form>
+                <?php
+            }
+            ?>
+        </p>
+      </div>
+      <div class="small-3 columns">
+        <?php
+          $a = new GlobalArea('Footer Social');
+          $a->display($c);
+        ?>
+      </div>
+    </div>
   </div><!-- /.container -->
  </div><!-- /.copyArea -->
 </footer>
 
 </div><!-- /getPageWrapperClass -->
 
-
+<script src="<?php echo $this->getThemePath(); ?>/js/vendor/jquery.js"></script>
+<script src="<?php echo $this->getThemePath(); ?>/js/foundation.min.js"></script>
+<script>
+  $(document).foundation();
+</script>
 <?php Loader::element('footer_required'); ?>
 </body>
 </html>
--- packages/sakan/themes/sakan/elements/header.php
+++ packages/sakan/themes/sakan/elements/header.php
@@ -4,10 +4,14 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php echo $html->css($view->getStylesheet('main.less'))?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <?php Loader::element('header_required', array('pageTitle' => $pageTitle));?>
+	  <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/css/normalize.css">
+	  <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/css/foundation.css">
+	  <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/css/typography.css">
+	  <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/css/styles.less">
+    <script src="<?php echo $this->getThemePath(); ?>/js/vendor/modernizr.js"></script>
   </head>
 
   <body>
@@ -15,30 +19,32 @@
 	<header>
 		<div class="navbar-inverse">
 			<div class="container">
-                <?php
-                  $a = new GlobalArea('Header Site Title');
-                  $a->display($c);
-                ?>
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#masthead" aria-expanded="false" aria-controls="navbar">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
+				<div class="row">
+	                <?php
+	                  $a = new GlobalArea('Header Site Title');
+	                  $a->display($c);
+	                ?>
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#masthead" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+					<div id="masthead" class="collapse navbar-collapse">
+	                <?php
+	                  $a = new GlobalArea('Header Search');
+	                  $a->display($c);
+	                ?>
+							<a href="#" onclick="$('#header_search_form').submit();"><span class="glyphicon glyphicon-search"></span></a>
+						</form>
+	                <?php
+	                  $a = new GlobalArea('Header Sub Navigation');
+	                  $a->display($c);
+	                ?>
+					</div><!-- /.nav-collapse -->
 				</div>
-				<div id="masthead" class="collapse navbar-collapse">
-                <?php
-                  $a = new GlobalArea('Header Search');
-                  $a->display($c);
-                ?>
-						<a href="#" onclick="$('#header_search_form').submit();"><span class="glyphicon glyphicon-search"></span></a>
-					</form>
-                <?php
-                  $a = new GlobalArea('Header Sub Navigation');
-                  $a->display($c);
-                ?>
-				</div><!-- /.nav-collapse -->
 			</div>
 		</div>
 
