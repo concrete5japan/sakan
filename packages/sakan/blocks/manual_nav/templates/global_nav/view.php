97239cb81f32189f9f56373494de8c3e05a784a8
01kawa <kmgrbgm@gmail>
2015-07-29 19:54:51 +0900

    change manual nav

---
+++ packages/sakan/blocks/manual_nav/templates/global_nav/view.php
@@ -0,0 +1,36 @@
+<?php defined('C5_EXECUTE') or die("Access Denied.");
+$c = Page::getCurrentPage();
+?>
+
+    <?php if (count($rows) > 0) {
+
+
+	$rows[0]['class'] = "nav-first";
+        foreach($rows as &$rowp)
+	{	
+	    if($rowp['internalLinkCID'] === $c->getCollectionID())
+	    {
+		$rowp['class'] .= " nav-selected";
+	    }
+	}
+    ?>
+    <nav id="globalNav" role="navigation" class="ccm-manual-nav-placeholder">
+    	<div class="container">
+		    <ul class="nav row text-center">
+		    <?php foreach ($rows as $row) { ?>
+			    <li class="col-sm-2 col-xs-6 <?php echo $row['class']?>">
+				<a href="<?php echo $row['linkURL'] ?>"><?php echo $row['title'] != null ? h($row['title']) : h($row['collectionName']); ?></a>
+			    </li>
+		    <?php } ?>
+		    </ul>
+    	</div>
+    </nav>
+<?php } else { ?>
+    <nav id="globalNav" role="navigation" class="ccm-manual-nav-placeholder">
+    	<div class="container">
+    		<div class="nav row text-center">
+        		<p class="text-left"><?php echo t('No nav Entered.'); ?></p>
+    		</div>
+    	</div>
+    </nav>
+<?php } ?>
--- packages/sakan/blocks/manual_nav/view.php
+++ packages/sakan/blocks/manual_nav/view.php
@@ -2,24 +2,25 @@
 $c = Page::getCurrentPage();
 ?>
 
-<div class="ccm-manual-nav-container ccm-block-manual-nav" >
-    <div class="ccm-manual-nav">
-        <div class="ccm-manual-nav-inner">
-
-        <?php if(count($rows) > 0) { ?>
-        <ul id="ccm-manual-nav-<?php echo $bID ?>">
-            <?php foreach($rows as $row) { ?>
-                <li>
-                    <a href="<?php echo $row['linkURL'] ?>" class="mega-link-overlay"><?php echo $row['title'] != null ? h($row['title']) : h($row['collectionName']);  ?></a>
-                </li>
-            <?php } ?>
-        </ul>
-        <?php } else { ?>
-        <div class="ccm-manual-nav-placeholder">
-            <p><?php echo t('No nav Entered.'); ?></p>
-        </div>
-        <?php } ?>
-        </div>
-
+    <?php if (count($rows) > 0) {
+	$rows[0]['class'] = "nav-first";
+        foreach($rows as &$rowp)
+	{	
+	    if($rowp['internalLinkCID'] === $c->getCollectionID())
+	    {
+		$rowp['class'] .= " nav-selected";
+	    }
+	}
+    ?>
+    <ul class="nav">
+    <?php foreach ($rows as $row) { ?>
+	    <li class="<?php echo $row['class']?>">
+		<a href="<?php echo $row['linkURL'] ?>"><?php echo $row['title'] != null ? h($row['title']) : h($row['collectionName']); ?></a>
+	    </li>
+    <?php } ?>
+    </ul>
+<?php } else { ?>
+    <div class="ccm-manual-nav-placeholder">
+        <p><?php echo t('No nav Entered.'); ?></p>
     </div>
-</div>
+<?php } ?>
