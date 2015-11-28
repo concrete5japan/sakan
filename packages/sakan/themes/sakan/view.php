0159a773d3f3d9e095e524a3e626d41df94e39cc
01kawa <kmgrbgm@gmail>
2015-11-28 15:01:55 +0900

    no message

--- packages/sakan/themes/sakan/view.php
+++ packages/sakan/themes/sakan/view.php
@@ -3,7 +3,21 @@ defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php'); ?>
 
 <?php
-print $innerContent;
+  $a = new GlobalArea('Bread nav');
+  $a->display($c);
 ?>
+<main>
+  <div class="container">
+   <div class="row">
+    <div class="main-contents <?php echo $c->getPageWrapperClass()?>">
+	<?php
+	print $innerContent;
+	?>
+    </div><!-- /.WrapperClass -->
+   </div><!-- /.row -->
+  </div><!-- /.container -->
+</main>
+
+
 
 <?php  $this->inc('elements/footer.php'); ?>
