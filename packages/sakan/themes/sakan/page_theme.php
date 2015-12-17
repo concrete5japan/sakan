7fdeaf03e19d8709ab87cef30e3dbc6c1e9073bd
01kawa <kmgrbgm@gmail>
2015-12-17 21:25:07 +0900

    edit page_theme.php

--- packages/sakan/themes/sakan/page_theme.php
+++ packages/sakan/themes/sakan/page_theme.php
@@ -8,7 +8,7 @@ class PageTheme extends Theme
 {
 
     public function registerAssets() {
-        //$this->providesAsset('javascript', 'bootstrap/*');
+        $this->providesAsset('javascript', 'bootstrap/*');
         $this->providesAsset('css', 'bootstrap/*');
         $this->providesAsset('css', 'blocks/form');
         $this->providesAsset('css', 'blocks/social_links');
