bd85a4d58c98af03f721e33335a96d0cc2a4a04a
yasui hiroaki <tenngutou@gmail.com>
2016-08-08 23:20:34 +0900

    pagelistカスタムテンプレート追加

---
+++ sakan/blocks/page_list/templates/news_list/view.php
@@ -0,0 +1,83 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+$th = Loader::helper('text');
+$c = Page::getCurrentPage();
+?>
+
+<div class="ccm-block-page-list-thumbnail-grid-wrapper">
+
+    <?php if ($pageListTitle): ?>
+        <div class="ccm-block-page-list-header">
+            <h1 class="page-title"><?php echo $pageListTitle?></h1>
+        </div>
+    <?php endif; ?>
+
+    <dl class="entry">
+
+    <?php foreach ($pages as $page):
+        $title = $th->entities($page->getCollectionName());
+        $url = $nh->getLinkToCollection($page);
+        $date = $c->getCollectionDateAdded('F j, Y');
+        $target = ($page->getCollectionPointerExternalLink() != '' && $page->openCollectionPointerExternalLinkInNewWindow()) ? '_blank' : $page->getAttribute('nav_target');
+        $target = empty($target) ? '_self' : $target;
+        $thumbnail = $page->getAttribute('thumbnail');
+        $topics = $page->getAttribute('project_topics');
+        $hoverLinkText = $title;
+        $description = $page->getCollectionDescription();
+        $description = $controller->truncateSummaries ? $th->wordSafeShortText($description, $controller->truncateChars) : $description;
+        $description = $th->entities($description);
+        if ($useButtonForLink) {
+            $hoverLinkText = $buttonLinkText;
+        }
+
+        ?>
+
+
+
+        <dt class="entry-date"><?php echo $date ?></dt>
+
+
+        <?php
+            if (count($topics) && is_array($topics)) {
+                foreach($topics as $topic) {
+                    echo "<dd class='entry-category'>" . $topic->getTreeNodeDisplayName() . "</dd>";
+                }
+            }
+        ?>
+
+        <dd class="entry-title">
+          <a href="<?php echo $url ?>" target="<?php echo $target ?>"><?php echo $title; ?></a>
+          <p><?php echo $description; ?></p>
+          <p><?php //var_dump($project_topics);?></p>
+
+
+        </dd>
+
+
+    <?php endforeach; ?>
+
+
+    </dl>
+
+    <?php if (count($pages) == 0): ?>
+        <div class="ccm-block-page-list-no-pages"><?php echo $noResultsMessage?></div>
+    <?php endif;?>
+
+</div>
+
+<?php if ($showPagination): ?>
+    <div class="text-center"><?php echo $pagination;?></div>
+<?php endif; ?>
+
+<?php if ($c->isEditMode() && $controller->isBlockEmpty()): ?>
+    <div class="ccm-edit-mode-disabled-item"><?php echo t('Empty Page List Block.')?></div>
+<?php endif; ?>
+
+
+
+
+
+
+
+
+
